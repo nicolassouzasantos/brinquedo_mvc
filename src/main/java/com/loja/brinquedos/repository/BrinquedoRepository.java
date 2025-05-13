7f9022b1bee8e5fc2671b69cb48b3eac0c900bd4
Nicolas Souza dos Santos <135378650+nicolassouzasantos@users.noreply.github.com>
2025-05-13 20:04:29 -0300

    Delete src directory

--- src/main/java/com/loja/brinquedos/repository/BrinquedoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.loja.brinquedos.repository;
-
-import com.loja.brinquedos.model.Brinquedo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BrinquedoRepository extends JpaRepository<Brinquedo, Long> {
-}
